76ffeef3cd7a25ef36454c3ff0be305235b0f64b
aleksey <azagarov@mail.ru>
2019-02-25 20:45:40 +0300

    Some more visual Changes

--- resources/lang/es/general_forms.php
+++ resources/lang/es/general_forms.php
@@ -2,16 +2,16 @@
 
 return [
 	'name' => 'Nombre',
-	'email' => 'E-Mail',
+	'email' => 'Correo electronico',
 	'phone' => 'Teléfono',
-	'subject' => 'Tema',
+	'subject' => 'Asunto',
 	'message' => 'Mensaje',
 
 
 	'name_placeholder' => 'Juan Carlos',
 	'email_placeholder' => 'juan_carlos@server.es',
 	'phone_placeholder' => '+12345678900',
-	'subject_placeholder' => 'Trato muy importante',
+	'subject_placeholder' => 'Un caso muy importante',
 	'message_placeholder' => '¡Hola!...',
 
     'button' => 'Enviar',
